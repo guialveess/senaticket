e3dc148b11f7c22a0acab57b581db881605ae420
Guilherme Alves <97guilherme.alves@gmail.com>
2024-04-11 00:10:09 -0300

    :tada: Commit inicial

---
+++ src/css/js/script.js
@@ -0,0 +1,10 @@
+const codigoCorreto = "12345";
+
+document.getElementById("acessarIngresso").addEventListener("click", function() {
+    const codigoUsuario = document.getElementById("codigoIngresso").value;
+    if (codigoUsuario === codigoCorreto) {
+        window.location.href = "ticket.html";
+    } else {
+        alert("Código incorreto. Tente novamente.");
+    }
+});
