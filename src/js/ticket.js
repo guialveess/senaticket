cf516bf76930c31ca5c556be4755c5dec25e5b39
Guilherme Alves <97guilherme.alves@gmail.com>
2024-04-22 22:54:44 -0300

    :boom: fix: Mudando o nome de Time para Jogo

--- src/js/ticket.js
+++ src/js/ticket.js
@@ -1,38 +1,8 @@
-// function obterParametroDaURL(nome) {
-//     const urlSearchParams = new URLSearchParams(window.location.search);
-//     return urlSearchParams.get(nome);
-//   }
-
-//   // Obter parâmetros da URL
-//   const nome = obterParametroDaURL('nome');
-//   const email = obterParametroDaURL('email');
-//   const time = obterParametroDaURL('time');
-
-//   // Substituir dados no card
-//   document.getElementById('nomeUsuario').textContent = nome;
-//   document.getElementById('emailUsuario').textContent = email;
-//   document.getElementById('meuBotao').textContent = 'Time: ' + time;
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const imagemBase64 = localStorage.getItem('imagem');
-
-//     if (imagemBase64) {
-//       const imagem = document.createElement('img');
-//       imagem.src = imagemBase64;
-
-//       const card = document.querySelector('.card');
-//       card.appendChild(imagem);
-//     }
-//   });
-
 document.addEventListener("DOMContentLoaded", async function () {
   const BASE_URL = "http://localhost:3000/"; // URL base da API
 
   const ingressoCode = localStorage.getItem("codIngresso");
   const emailUser = localStorage.getItem("email");
-
-  console.log(ingressoCode);
-  console.log(emailUser);
 
   if (!ingressoCode) {
     console.error("Código do ingresso não encontrado.");
@@ -40,27 +10,24 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 
   if (!emailUser) {
-    console.error("Email não encontrado.");
+    console.error("email não encontrado.");
     return; // Encerra se não houver código armazenado
   }
-  console.log("Passou validações");
 
   try {
     const response = await axios.post(BASE_URL + "ingressUser", {
       email: emailUser,
       codigo: ingressoCode,
     });
-    console.log("Passou request");
 
     const data = response.data;
-    console.log(data);
 
     // Atualiza os dados na página
     document.getElementById("name").getElementsByTagName("h1")[0].textContent =
       data.nome;
     document.getElementById("email").textContent = data.email;
     document.querySelector(".button-wrapper button").textContent =
-      "Time: " + data.jogo;
+      "Jogo: " + data.jogo;
     document.querySelector(".button-wrapper2 button").textContent =
       "#" + data.codIngresso;
 
